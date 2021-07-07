89aa9896e87a02319f13416a46fdb43a09a849cd
Alexandr S <juzgun73@yandex.ru>
2021-07-07 22:29:20 +0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://e7.pngegg.com/pngimages/543/681/png-clipart-computer-icons-icon-design-business-brand-awareness-icon-company-text.png" alt="company logo"></img>
+            header
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <img src="https://media.istockphoto.com/vectors/retro-city-wide-banner-vector-id466052873" alt="main pic"></img>
+            <div>
+                <img src="https://e7.pngegg.com/pngimages/543/681/png-clipart-computer-icons-icon-design-business-brand-awareness-icon-company-text.png" alt="main pic" className="ava"></img>
+                ava + descr
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    Posts
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+            Main content
+        </div>
+    )
+}
+
+export default Profile;
